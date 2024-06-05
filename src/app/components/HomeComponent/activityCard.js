503baa00049963f9feacae2597c429ff2a9a39a7
unknown <maxencemazeau@outlook.fr>
2024-06-05 12:25:23 -0400

    Timer condition activity card

--- src/app/components/HomeComponent/activityCard.js
+++ src/app/components/HomeComponent/activityCard.js
@@ -12,18 +12,18 @@ import { getActivity } from "../../axiosPath/axiosPath";
 import axios from 'axios'
 
 export default function ActivityCard() {
-    
+
     const [timer, setTimer] = useState(false)
     const [timerText, setTimerText] = useState("START")
     const [activityList, setActivityList] = useState()
 
     useEffect(() => {
         const fetchData = async () => {
-              const response = await axios.get(getActivity);
-              setActivityList(response.data);
-          };
-      
-          fetchData();
+            const response = await axios.get(getActivity);
+            setActivityList(response.data);
+        };
+
+        fetchData();
     }, [])
     console.log(activityList)
 
@@ -32,72 +32,49 @@ export default function ActivityCard() {
     })
 
     const changeTimer = () => {
-        if(timer == false) {
+        if (timer == false) {
             setTimer(true);
             setTimerText("STOP")
         } else {
             setTimer(false);
             setTimerText("START")
         }
-        
+
     }
 
     return (
         <Container maxWidth="md" sx={{ paddingRight: 2, paddingLeft: 2, paddingTop: 2 }}>
-        {activityList && activityList.map(activities => (
-            <Card key={activities.ActivityID} variant="outlined" sx={{ marginBottom: 3, borderRadius: 4 }}>
-            <CardContent>
-                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ marginTop: 0.5 }}>
-                    <Grid item xs={9} sm={10}>
-                        <Typography variant="h6" sx={{fontFamily:"Poppins_700Bold"}} gutterBottom>{activities.ActivityName}</Typography>
-                        <Typography variant="body1" sx={{ marginTop: 1, fontFamily:'Poppins_400Regular' }} gutterBottom>asdsasd</Typography>
-                    </Grid>
-                    <Grid item xs={2} sm={1}>
-                        <Link href={{ pathname: "/pages/activity/activityDetail", params: { lastPage:"/pages/home/reduxHomeProvider"} }}>
-                            <Button sx={{ bgcolor: "#DD7A34", borderRadius: 25, height: 50 }}>
-                                <ArrowForwardIosIcon  sx={{ color: "white" }} />
-                            </Button>
-                        </Link>
-                    </Grid>
-                </Grid>
-                <Divider></Divider>
-                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ marginTop: 0.5 }}>
-                    <Grid item xs={9}>
-                        <Typography variant="body1" color={timer == true ? 'red' : 'green'} sx={{ fontFamily:"Poppins_700Bold" }} onClick={() => changeTimer()}>{timerText}</Typography>
-                    </Grid>
-                    <Grid item xs={2}>
-                        <Typography variant="body1" sx={{fontFamily:"Poppins_400Regular"}}>10:00:00</Typography>
-                    </Grid>
-                </Grid>
-            </CardContent>
-        </Card>
-        ))}
-            <Card variant="outlined" sx={{ marginBottom: 3, borderRadius: 4 }}>
-                <CardContent>
-                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ marginTop: 0.5 }}>
-                        <Grid item xs={9} sm={10}>
-                            <Typography variant="h6" sx={{fontFamily:"Poppins_700Bold"}} gutterBottom>MyActivity</Typography>
-                            <Typography variant="body1" sx={{ marginTop: 1, fontFamily:'Poppins_400Regular' }} gutterBottom>asdsasd</Typography>
+            {activityList && activityList.map(activities => (
+                <Card key={activities.ActivityID} variant="outlined" sx={{ marginBottom: 3, borderRadius: 4 }}>
+                    <CardContent>
+                        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ marginTop: 0.5 }}>
+                            <Grid item xs={9} sm={10}>
+                                <Typography variant="h6" sx={{ fontFamily: "Poppins_700Bold" }} gutterBottom>{activities.ActivityName}</Typography>
+                                <Typography variant="body1" sx={{ marginTop: 1, fontFamily: 'Poppins_400Regular' }} gutterBottom>asdsasd</Typography>
+                            </Grid>
+                            <Grid item xs={2} sm={1}>
+                                <Link href={{ pathname: "/pages/activity/activityDetail", params: { lastPage: "/pages/home/reduxHomeProvider" } }}>
+                                    <Button sx={{ bgcolor: "#DD7A34", borderRadius: 25, height: 50 }}>
+                                        <ArrowForwardIosIcon sx={{ color: "white" }} />
+                                    </Button>
+                                </Link>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={2} sm={1}>
-                            <Link href={{ pathname: "/pages/activity/activityDetail", params: { lastPage:"/pages/home/reduxHomeProvider"} }}>
-                                <Button sx={{ bgcolor: "#DD7A34", borderRadius: 25, height: 50 }}>
-                                    <ArrowForwardIosIcon  sx={{ color: "white" }} />
-                                </Button>
-                            </Link>
-                        </Grid>
-                    </Grid>
-                    <Divider></Divider>
-                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ marginTop: 0.5 }}>
-                        <Grid item xs={9}>
-                            <Typography variant="body1" color={timer == true ? 'red' : 'green'} sx={{ fontFamily:"Poppins_700Bold" }} onClick={() => changeTimer()}>{timerText}</Typography>
-                        </Grid>
-                        <Grid item xs={2}>
-                            <Typography variant="body1" sx={{fontFamily:"Poppins_400Regular"}}>10:00:00</Typography>
-                        </Grid>
-                    </Grid>
-                </CardContent>
-            </Card>
+                        {activities.Timer &&
+                            <>
+                                <Divider></Divider>
+                                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ marginTop: 0.5 }}>
+                                    <Grid item xs={9}>
+                                        <Typography variant="body1" color={timer == true ? 'red' : 'green'} sx={{ fontFamily: "Poppins_700Bold" }} onClick={() => changeTimer()}>{timerText}</Typography>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                        <Typography variant="body1" sx={{ fontFamily: "Poppins_400Regular" }}>10:00:00</Typography>
+                                    </Grid>
+                                </Grid>
+                            </>}
+                    </CardContent>
+                </Card>
+            ))}
         </Container>
     )
 
