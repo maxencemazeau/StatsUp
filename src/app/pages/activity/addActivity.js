d779b185318ee0eaeb63a6557d26dc9b7c6219b3
unknown <maxencemazeau@outlook.fr>
2024-06-20 16:51:05 -0400

    Debut add activity

--- src/app/pages/activity/addActivity.js
+++ src/app/pages/activity/addActivity.js
@@ -8,6 +8,8 @@ import { useFonts, Poppins_400Regular, Poppins_700Bold } from '@expo-google-font
 import Checkbox from "@mui/material/Checkbox"
 import Select from "@mui/material/Select"; import MenuItem from "@mui/material/MenuItem"
 import InputLabel from "@mui/material/InputLabel"; import FormControl from "@mui/material/FormControl"
+import axios from "axios"
+import { addActivity } from "../../axiosPath/axiosPath"
 
 export default function AddActivity() {
 
@@ -26,7 +28,10 @@ export default function AddActivity() {
     const [activateGoal, setActivateGoal] = useState(false)
     const [goalName, setGoalName] = useState(false)
     const [timerOption, setTimerOption] = useState(false)
-
+    const [newActivity, setNewActivity] = useState({
+        activityName: "",
+        userId : 0
+    })
     const changeActive = () => {
         setActivateGoal(prevState => !prevState)
     }
@@ -45,6 +50,11 @@ export default function AddActivity() {
         }
     }
 
+    const addNewActivity = async() => {
+        console.log(newActivity)
+        const response = await axios.post(addActivity, {params :{ ActivityName: newActivity.activityName, UserId : 1 }})
+    }
+
     const [fontsLoad] = useFonts({
         Poppins_400Regular, Poppins_700Bold,
     })
@@ -52,53 +62,53 @@ export default function AddActivity() {
     return (
         <>
             <View>
-                            <Typography variant="h6" sx={{ fontFamily: "Poppins_700Bold" }}>Create a new activity</Typography>
-                            <TextField id="outlined-basic" label="Name" color={"warning"} variant="outlined" margin="normal" />
-                            <Grid direction={"row"} container sx={{ paddingTop: 1 }}>
-                                <Grid item xs={6} sx={{ marginBottom: 1 }}>
-                                    <Typography>Timer options ?</Typography>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <Checkbox sx={{ padding: 0 }} onChange={changeTimerOption} />
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <Typography>Link this activity to a goal ?</Typography>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <Checkbox sx={{ padding: 0 }} onChange={changeActive} />
-                                </Grid>
+                <Typography variant="h6" sx={{ fontFamily: "Poppins_700Bold" }}>Create a new activity</Typography>
+                <TextField id="outlined-basic" label="Name" color={"warning"} variant="outlined" margin="normal" onChange={(e) => setNewActivity({activityName : e.target.value})}/>
+                <Grid direction={"row"} container sx={{ paddingTop: 1 }}>
+                    <Grid item xs={6} sx={{ marginBottom: 1 }}>
+                        <Typography>Timer options ?</Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <Checkbox sx={{ padding: 0 }} onChange={changeTimerOption} />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography>Link this activity to a goal ?</Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <Checkbox sx={{ padding: 0 }} onChange={changeActive} />
+                    </Grid>
+                </Grid>
+                {activateGoal &&
+                    <>
+                        <FormControl fullWidth sx={{ marginTop: 1 }}>
+                            <InputLabel id="demo-simple-select-label" color={"warning"}>Linked goal</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={age}
+                                label="Linked goal"
+                                color={"warning"}
+                                onChange={handleChange}
+                            >
+                                <MenuItem value={0}>New goal</MenuItem>
+                                <Divider />
+                                {goal.map(goals =>
+                                    <MenuItem key={goals.id} value={goals.name}>{goals.name}</MenuItem>
+                                )}
+                            </Select>
+                        </FormControl>
+                        {goalName && <TextField id="outlined-basic" label="Name" color={"warning"} variant="outlined" margin="normal" />}
+                        <Grid container spacing={3}>
+                            <Grid item xs={6}>
+                                <TextField id="outlined-basic" label="Time frame" variant="outlined" color={"warning"} margin="normal" />
                             </Grid>
-                            {activateGoal &&
-                                <>
-                                    <FormControl fullWidth sx={{ marginTop: 1 }}>
-                                        <InputLabel id="demo-simple-select-label" color={"warning"}>Linked goal</InputLabel>
-                                        <Select
-                                            labelId="demo-simple-select-label"
-                                            id="demo-simple-select"
-                                            value={age}
-                                            label="Linked goal"
-                                            color={"warning"}
-                                            onChange={handleChange}
-                                        >
-                                            <MenuItem value={0}>New goal</MenuItem>
-                                            <Divider />
-                                            {goal.map(goals =>
-                                                <MenuItem key={goals.id} value={goals.name}>{goals.name}</MenuItem>
-                                            )}
-                                        </Select>
-                                    </FormControl>
-                                    {goalName && <TextField id="outlined-basic" label="Name" color={"warning"} variant="outlined" margin="normal" />}
-                                    <Grid container spacing={3}>
-                                        <Grid item xs={6}>
-                                            <TextField id="outlined-basic" label="Time frame" variant="outlined" color={"warning"} margin="normal" />
-                                        </Grid>
-                                        <Grid item xs={6}>
-                                            <TextField id="outlined-basic" label="Frequency" variant="outlined" color={"warning"} margin="normal" />
-                                        </Grid>
-                                    </Grid>
-                                </>
-                            }
-                            <Button variant={"contained"} sx={{ bgcolor: "#DD7A34", marginTop: 1, height:50 }} color={"warning"} disableElevation>Save</Button>
+                            <Grid item xs={6}>
+                                <TextField id="outlined-basic" label="Frequency" variant="outlined" color={"warning"} margin="normal" />
+                            </Grid>
+                        </Grid>
+                    </>
+                }
+                <Button variant={"contained"} sx={{ bgcolor: "#DD7A34", marginTop: 1, height: 50 }} color={"warning"} disableElevation onClick={addNewActivity}>Save</Button>
             </View>
         </>
     )
